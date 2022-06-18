0fa87e99156867f399d0c8ee0f0c9fd948821c5c
Pavan GA <91429674+pavan-10-7@users.noreply.github.com>
2022-06-18 23:31:38 +0530

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -27,17 +27,3 @@ function SlideShow(n) {
   circles[slidePosition-1].className += " enable";
 } 
 
-var slidePosition = 0;
-SlideShow();
-
-function SlideShow() {
-  var i;
-  var slides = document.getElementsByClassName("Containers");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slidePosition++;
-  if (slidePosition > slides.length) {slidePosition = 1}
-  slides[slidePosition-1].style.display = "block";
-  setTimeout(SlideShow, 2000); // Change image every 2 seconds
-} 
